b918bd3bb8878635507e12e858864865342fd029
JoaoVictorRodrigues <joaovrs1@al.insper.edu.br>
2020-12-04 22:41:12 -0300

    alterando o projeto.py

--- projeto2.py
+++ projeto2.py
@@ -3,16 +3,9 @@ import pyspark
 import math
 import matplotlib.pyplot as plt
 from PIL import Image
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+#from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import pandas as pd
 
-sc = pyspark.SparkContext(appName="jojo")
-
-rdd = sc.sequenceFile("part-00000")
-
-rdd.count()
-
-result = rdd.take(10)
 
 def conta_palavras_doc(item):
     url, conteudo = item
@@ -31,10 +24,6 @@ def conta_uma_palavra(item, palavra):
         return [item]
     return []
 
-N = rdd.count()
-
-DOC_COUNT_MIN = 5
-DOC_COUNT_MAX = 0.7*N
 
 def filtra_doc_freq(item):
     contagem = item[1]
@@ -56,54 +45,49 @@ def relevancia(item):
     relevancia = freq*idf
     return (palavra, relevancia)
     
-if __name__ == "__main__":
-    sc = pyspark.SparkContext(appName="jojo")
-    rdd = sc.sequenceFile("part-00000")
+sc = pyspark.SparkContext(appName="Projeto2")
+rdd = sc.sequenceFile("s3://megadados-alunos/web-brasil")
 
-    Rakin = rdd.filter(lambda x:"rakin" in x[1])
-    Yoda = rdd.filter(lambda x:"yoda" in x[1])
-    inter = Rakin.intersection(Yoda)
+N = rdd.count()
 
-    rdd_Rakin = Rakin.flatMap(conta_uma_palavra).reduceByKey(junta_contagens)
-    rdd_Yoda = Yoda.flatMap(conta_uma_palavra).reduceByKey(junta_contagens)
-    rdd_inter = inter.flatMap(conta_uma_palavra).reduceByKey(junta_contagens)
+DOC_COUNT_MIN = 10
+DOC_COUNT_MAX = 0.7*N
 
-    rdd_doc_rakin = Rakin.flatMap(conta_palavras_doc).reduceByKey(junta_contagens)
-    rdd_doc_yoda = Yoda.flatMap(conta_palavras_doc).reduceByKey(junta_contagens)
-    rdd_doc_inter = inter.flatMap(conta_palavras_doc).reduceByKey(junta_contagens)
-
-    rdd_doc_rakin_freq_filtrado = rdd_doc_rakin.filter(filtra_doc_freq)
-    rdd_doc_yoda_freq_filtrado = rdd_doc_yoda.filter(filtra_doc_freq)
-    rdd_doc_inter_freq_filtrado = rdd_doc_inter.filter(filtra_doc_freq)
-
-    rakin_rdd_idf = rdd_doc_rakin_freq_filtrado.map(computa_idf)
-    yoda_rdd_idf = rdd_doc_yoda_freq_filtrado.map(computa_idf)
-    inter_rdd_idf = rdd_doc_inter_freq_filtrado.map(computa_idf)
-
-    rakin_rdd_freq = rdd_doc_rakin_freq_filtrado.map(computa_freq)
-    yoda_rdd_freq = rdd_doc_yoda_freq_filtrado.map(computa_freq)
-    inter_rdd_freq = rdd_doc_inter_freq_filtrado.map(computa_freq)
+Rakin = rdd.filter(lambda x:"rakin" in x[1])
+rdd_Rakin = Rakin.flatMap(conta_uma_palavra).reduceByKey(junta_contagens)
+rdd_doc_rakin = Rakin.flatMap(conta_palavras_doc).reduceByKey(junta_contagens)
+rdd_doc_rakin_freq_filtrado = rdd_doc_rakin.filter(filtra_doc_freq)
+rakin_rdd_idf = rdd_doc_rakin_freq_filtrado.map(computa_idf)
+rakin_rdd_freq = rdd_doc_rakin_freq_filtrado.map(computa_freq)
+rdd_rakin_join = rakin_rdd_freq.join(rakin_rdd_idf)
+relevancia_rakin = rdd_rakin_join.map(relevancia)
+rakin_list = relevancia_rakin.takeOrdered(100, key=lambda x: -x[1])
+rakin_data = pd.DataFrame(data = rakin_list, columns=["palavra", "relevancia"])
+rakinBD = rakin_data.to_csv("s3://megadados-alunos/joao-jose/rakin.csv", index=False)
 
 
-    rdd_rakin_join = rakin_rdd_freq.join(rakin_rdd_idf)
-    rdd_yoda_join = yoda_rdd_freq.join(yoda_rdd_idf)
-    rdd_inter_join = inter_rdd_freq.join(inter_rdd_idf)
+Yoda = rdd.filter(lambda x:"yoda" in x[1])
+rdd_Yoda = Yoda.flatMap(conta_uma_palavra).reduceByKey(junta_contagens)
+rdd_doc_yoda = Yoda.flatMap(conta_palavras_doc).reduceByKey(junta_contagens)
+rdd_doc_yoda_freq_filtrado = rdd_doc_yoda.filter(filtra_doc_freq)
+yoda_rdd_idf = rdd_doc_yoda_freq_filtrado.map(computa_idf)
+yoda_rdd_freq = rdd_doc_yoda_freq_filtrado.map(computa_freq)
+rdd_yoda_join = yoda_rdd_freq.join(yoda_rdd_idf)
+relevancia_yoda = rdd_yoda_join.map(relevancia)
+yoda_list = relevancia_yoda.takeOrdered(100, key=lambda x: -x[1])
+yoda_data = pd.DataFrame(data = yoda_list, columns=["palavra", "relevancia"])
+yodaBD = yoda_data.to_csv("s3://megadados-alunos/joao-jose/yoda.csv", index=False)
 
-    relevancia_rakin = rdd_rakin_join.map(relevancia)
-    relevancia_yoda = rdd_yoda_join.map(relevancia)
-    relevancia_inter = rdd_inter_join.map(relevancia)
 
-    
-    rakin_list = relevancia_rakin.takeOrdered(100, key=lambda x: -x[1])
-    yoda_list = relevancia_yoda.takeOrdered(100, key=lambda x: -x[1])
-    inter_list = relevancia_inter.takeOrdered(100, key=lambda x: -x[1])
-
-
-    rakin_data = pd.DataFrame(data = relevancia_rakin, columns=["palavra", "relevancia"])
-    yoda_data = pd.DataFrame(data = relevancia_yoda, columns=["palavra", "relevancia"])
-    inter_data = pd.DataFrame(data = relevancia_inter, columns=["palavra", "relevancia"])
-
-    rakinBD = rakin_data.to_csv("s3linknosso", index=False)
-    yodaBD = yoda_data.to_csv("s3linknosso", index=False)
-    interBD = inter_data.to_csvI("s3linknosso", index=False)
+inter = rdd.filter(lambda x:"yoda" in x[1] and "rakin" in x[1])
+rdd_inter = inter.flatMap(conta_uma_palavra).reduceByKey(junta_contagens)  
+rdd_doc_inter = inter.flatMap(conta_palavras_doc).reduceByKey(junta_contagens)  
+rdd_doc_inter_freq_filtrado = rdd_doc_inter.filter(filtra_doc_freq)
+inter_rdd_idf = rdd_doc_inter_freq_filtrado.map(computa_idf) 
+inter_rdd_freq = rdd_doc_inter_freq_filtrado.map(computa_freq) 
+rdd_inter_join = inter_rdd_freq.join(inter_rdd_idf)
+relevancia_inter = rdd_inter_join.map(relevancia) 
+inter_list = relevancia_inter.takeOrdered(100, key=lambda x: -x[1])
+inter_data = pd.DataFrame(data = inter_list, columns=["palavra", "relevancia"])
+interBD = inter_data.to_csv("s3://megadados-alunos/joao-jose/inter.csv", index=False)
 
